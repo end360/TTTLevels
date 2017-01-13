9c6114cac9cae06a532e55a18909925fd82a08a9
Evan Orr <end360@users.noreply.github.com>
2017-01-13 09:55:31 -0500

    Update cl_init.lua

--- lua/levels/cl_init.lua
+++ lua/levels/cl_init.lua
@@ -1,6 +1,6 @@
-local myXP = myXP or 0
-local myLevel = myLevel or 1
-local levelColors = {
+Levels.myXP = Levels.myXP or 0
+Levels.myLevel = Levels.myLevel or 1
+Levels.LevelColors = {
 	Color(235, 0, 0),
 	Color(0, 235, 0),
 	Color(0, 0, 235),
@@ -10,40 +10,38 @@ local levelColors = {
 	Color(0, 141, 255)
 }
 
-net.Receive("TTTXP_GetXP", function(len)
-		myXP = net.ReadInt(32)
-		myLevel = net.ReadInt(32)
+net.Receive("Levels_GetXP", function(len)
+		Levels.myXP = net.ReadInt(32)
+		Levels.myLevel = net.ReadInt(32)
 end)
-net.Receive("TTTXP_AddXP", function(len)
+net.Receive("Levels_AddXP", function(len)
 		local toadd = net.ReadInt(32)
-		addXP(toadd)
+		Levels.addXP(toadd)
 end)
 local w = ScrW()*.2604167
 local h = ScrH()*.037
 local y = 50
 local a = 255
-local addxps = {}
+Levels.addxps = {}
 local times = CurTime()
 function addXP(amt)
-	myXP = myXP + amt
+	Levels.myXP = Levels.myXP + amt
 	
-	while myXP >= xpForLevel do
-		myXP = myXP - xpForLevel
-		myLevel = myLevel + 1
-		table.insert(addxps, {"Level Up", h+60+(#addxps*18), 255})
+	while Levels.myXP >= Levels.xpForLevel do
+		Levels.myXP = Levels.myXP - Levels.xpForLevel
+		Levels.myLevel = Levels.myLevel + 1
+		Levels.addxps[#Levels.addxps + 1] = {"Level Up", h+60+(#Levels.addxps*18), 255}
 	end
 	times = CurTime()
-	table.insert(addxps, {amt, h+60+(#addxps*18), 255})
+	Levels.addxps[#Levels.addxps + 1] = {amt, h+60+(#Levels.addxps*18), 255}
 end
 local function drawAddXP()
+	if hook.Call("HUDShouldDraw", "LevelsAddXP") == false then return end
 	if #addxps > 0 and times+5 > CurTime() then
-		local i = 0
-		for k,xp in pairs(addxps) do
-			if i > 10 then break end 
-			draw.SimpleText("+ "..xp[1], "Trebuchet24", 45, xp[2], Color(255,255,255,xp[3]), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
-			xp[2] = math.Clamp(xp[2] - .10, h+20, 999999)
-			xp[3] = math.Clamp(xp[3] - 1.125, 0, 255)
-			i = i + 1
+		for i=0, #addxps do
+			draw.SimpleText("+ "..Levels.addxps[i][1], "Trebuchet24", 45, Levels.addxp[i][2], Color(255,255,255,Levels.addxp[i][3]), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+			Levels.addxp[i][2] = math.Clamp(Levels.addxp[i][2] - .10, h+20, 999999)
+			Levels.addxp[i][3] = math.Clamp(Levels.addxp[i][3] - 1.125, 0, 255)
 		end
 	elseif #addxps > 0 and times+5 < CurTime() then
 		addxps = {}
@@ -53,7 +51,12 @@ hook.Add("HUDPaint", "TTTXP_PaintXPUpdate", drawAddXP)
 local twenty = ScrH()*.019
 local twentyfive = ScrH()*.023
 local ten = ScrH()*.0093
+local levelColors = Levels.levelColors
 local function HUDPaint()
+	if hook.Call("HUDShouldDraw", "LevelsXPBar") == false then return end
+	local myXP = Levels.myXP
+	local xpForLevel = Levels.xpForLevel
+	local myLevel = Levels.myLevel
 	draw.RoundedBox(0, twenty, twenty, w, h, Color(96, 96, 96, 225) )
 	draw.RoundedBox(0, twentyfive, twentyfive, w*math.Clamp(myXP/xpForLevel,0,.98), h-ten, levelColors[(myLevel)%(#levelColors-1)] or Color(0,191,255))
 	draw.SimpleText("Level: "..myLevel,"Trebuchet24",(w+twentyfive)/2,h,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
