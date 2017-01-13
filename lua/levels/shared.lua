70b777e1edb0e1bd4696b86854002d335dd48741
Evan Orr <end360@users.noreply.github.com>
2017-01-13 09:57:15 -0500

    Update shared.lua

--- lua/levels/shared.lua
+++ lua/levels/shared.lua
@@ -1,17 +1,17 @@
-xpForLevel = 100
-xpForTKill = 10
-xpForInnoKill = 10
-xpLossForTK = 0 // Set to anything above 0 to lose XP
+Levels.xpForLevel = 100
+Levels.xpForTKill = 10
+Levels.xpForInnoKill = 10
+Levels.xpLossForTK = 0 -- Set to anything above 0 to lose XP
 
-// Pointshop On Levelup, Remove /* and */ to enable
+-- Pointshop (1) points on level-up, Remove /* and */ to enable
 /*
-  pointsForLevelup = 1000
-  local function givePointshopReward(ply, lvl)
-    if SERVER then
-      ply:PS_GivePoints(pointsForLevelup)
-    else
-      // Do something
-    end
+local pointsForLevelup = 1000
+local function givePointshopReward(ply, lvl)
+  if SERVER then
+    ply:PS_GivePoints(pointsForLevelup)
+  else
+    // HUD Stuff would go here
+  end
 end
-hook.Add("Levels_LevelUp", "TTTXP_PSReward", givePointshopReward)
+hook.Add("Levels_LevelUp", "Levels_PSReward", givePointshopReward)
 */
